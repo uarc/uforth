e2ca8d295834237bfb34d1e26e2841b20344d7e2
Geordon Worley <vadixidav@gmail.com>
2016-07-18 21:28:22 -0400

    add some code

--- src/main.asm
+++ src/main.asm
@@ -1,19 +1,45 @@
-# Select bus 0
+:INIT
+jmpi:QUIT
+
+:CONTINUE_NAME
+i:8
+i:"CONTINUE
+
+:CONTINUE
+ld0i
+:CONTINUE-
+jmpi:CONTINUE-
+
+:DEFER_NAME
+i:5
+i:"DEFER
+
+:DEFER
+
+:QUIT_NAME
+i:4
+i:"QUIT
+
+:QUIT
+ld0i
+# Reset the processor, immediately moving to the location after this.
+$QUIT+ .QUIT+ read0 read0 reset
+:QUIT+
 0 slb
-# Set the interrupt PC and DC for bus 0
-$interrupt iset:interrupt
-# Enable interrupts
+$KEYBOARD_INTERRUPT iset:KEYBOARD_INTERRUPT
 ien
-# Create a loop that will go on effectively forever
--1 loopi:+
-continue
-:+
+jenteri:CONTINUE
 
 # Interrupt tag
-:interrupt
+:KEYBOARD_INTERRUPT
 # Inside the interrupt, echo the value back
 cv1 send
 return
+
+:DEFER
+
+:CONTINUE_XT
+.CONTINUE $CONTINUE $CONTINUE_NAME i:0b0
 
 # Align the segments
 alignp:0x3C,2048
