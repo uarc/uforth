884718154fb52e37052405981872b67751c2a96e
Geordon Worley <vadixidav@gmail.com>
2016-07-20 21:12:46 -0400

    move some stuff around and add some words

--- src/main.asm
+++ src/main.asm
@@ -1,10 +1,16 @@
 :INIT
 jmpi:QUIT
 
+:base_NAME
+i:4
+i:"base
+:base
+$base_DSP read0 write3
+0x30 jumpi:DEFER
+
 :CONTINUE_NAME
 i:8
 i:"CONTINUE
-
 :CONTINUE
 ld0i
 :CONTINUE-
@@ -13,33 +19,33 @@ jmpi:CONTINUE-
 :DEFER_NAME
 i:5
 i:"DEFER
-
 :DEFER
+
+:HEREP!_NAME
+i:6
+i:"HEREP!
+:HEREP!
+
+:NUMBER_NAME
+i:6
+i:"NUMBER
+:NUMBER
 
 :QUIT_NAME
 i:4
 i:"QUIT
-
 :QUIT
 ld0i
 # Reset the processor, immediately moving to the location after this.
 $QUIT+ .QUIT+ read0 read0 reset
 :QUIT+
-0 slb
-$KEYBOARD_INTERRUPT iset:KEYBOARD_INTERRUPT
-ien
 jenteri:CONTINUE
-
-# Interrupt tag
-:KEYBOARD_INTERRUPT
-# Inside the interrupt, echo the value back
-cv1 send
-return
-
-:DEFER
 
 :CONTINUE_XT
 .CONTINUE $CONTINUE $CONTINUE_NAME i:0b0
+
+:base_XT
+.base :base_DSP fill:10,1 $base_NAME i:0b1
 
 # Align the segments
 alignp:0x3C,2048
