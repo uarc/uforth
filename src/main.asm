6ddb00b568c99ee314e8886095a9cfd07fb0fbd8
Geordon Worley <vadixidav@gmail.com>
2016-08-13 16:59:08 -0400

    switch stuff to callri and implement STREQ

--- src/main.asm
+++ src/main.asm
@@ -24,7 +24,7 @@ calli:STATE bz:+
 # Set dc0 to the back stack.
 push0 calli:hereb reads set0
 # Get the string address and length on the stack.
-calli:pp reads dup calli:BL calli:DWORD
+callri:pp reads dup callri:BL callri:DWORD
 # Place the string length in an immediate location in the loop.
 writepri:+++
 # Iterate through every dictionary entry.
@@ -40,11 +40,11 @@ iloop:+
     dup
     # Lookup and get expanded string addr count combination from dictionary entry.
     # Offset of 2 on dictionary entry.
-    rareadi0:2 calli:COUNT
+    rareadi0:2 callri:COUNT
     # Get this string length
     imm32 +++ pfill:0,4
     # Compare the two strings
-    calli:STREQ bz:++
+    callri:STREQ bz:++
         # Found a match, so return the xt.
         drop read0:0
         # Set the carry bit to 1 if this is an immediate instruction (this is for internal use).
@@ -52,6 +52,7 @@ iloop:+
         # Restore state.
         pop0 discard return
     ++
+    # Need a continue here because we cannot branch to the end of the loop.
     continue
 +
 
@@ -415,6 +416,17 @@ iloop:+
 
 :STREQ_name $5 $"STREQ
 :STREQ
+copy1 beq:+
+    ddrop drop imm32:0 return
++
+push0 push1 rot2 set0 rot1 set1
+loop:+
+    read0:1 read1:1 beq:++
+        pop1 pop0 discard imm32:0 return
+    ++
+    continue
++
+pop1 pop0 imm32:1 return
 
 :THEN_name $4 $"THEN
 :THEN
